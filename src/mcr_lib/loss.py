e81157fef9d719a5102880cff6e40cc9040d9572
Varun Bharadwaj <varunbharadwaj@berkeley.edu>
2024-06-07 13:13:21 -0700

    Made small updates to loss

--- src/mcr_lib/loss.py
+++ src/mcr_lib/loss.py
@@ -5,8 +5,7 @@ def R(Z: torch.Tensor, eps_sq: float) -> torch.Tensor:
     d, m = Z.shape
     I = torch.eye(d)
     log_det_matrix = I + d/(m * eps_sq) * Z.matmul(Z.T)
-    log_det_matrix = log_det_matrix.detach().numpy()
-    res = 1/2 * sum(np.log(np.linalg.eigvals(log_det_matrix)))
+    res = 1/2 * sum(torch.log(torch.linalg.eigvalsh(log_det_matrix)))
     return res
     
 def R_c(Z: torch.Tensor, Pi: torch.Tensor, eps_sq: float) -> torch.Tensor:
@@ -17,8 +16,7 @@ def R_c(Z: torch.Tensor, Pi: torch.Tensor, eps_sq: float) -> torch.Tensor:
 
     trace = torch.vmap(torch.trace)(Pi)
     log_det_matrix = I + d/(trace * eps_sq) * Z.matmul(Pi).matmul(Z.T)
-    log_det_matrix = log_det_matrix.detach().numpy()
-    res = sum(trace / (2 * m) * sum(np.log(np.linalg.eigvals(log_det_matrix))))
+    res = sum(trace / (2 * m) * sum(torch.log(torch.linalg.eigvalsh(log_det_matrix))))
 
     return res
 
@@ -30,7 +28,7 @@ def forward(X: torch.Tensor, Y: torch.Tensor) -> torch.Tensor:
     num_categories = max(Y) + 1
     Pi = np.zeros((num_categories, num_samples, num_samples))
     for j in range(len(Y)):
-        Pi[Y[j], j, j] = 1
+        Pi[Y[j], j] = 1
         
     Pi = torch.from_numpy(Pi).float()
     res = R(Z, eps_sq) - R_c(Z, Pi, eps_sq)
